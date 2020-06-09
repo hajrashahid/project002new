37923924e9b3b9f750ef958296b5796ee55c15d6
hajrashahid <hajrashahid92@gmail.com>
2020-06-10 01:19:11 +0500

    changed index.html

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <br/>
+          <p>Hello World by Hajra Shahid</p>
         </p>
         <a
           className="App-link"
